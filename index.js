b394a7d467483a3a51d107a93c096a48b77162ae
Test <adrienlegeleu@gmail.com>
2023-12-03 12:09:29 +0100

    quiz-js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+class Question{
+  constructor(text,choices,answer){
+    this.text=text;
+    this.choices=choices;
+    this.answer=answer;
+  }
+  isCorrectAnswer(choice){
+    return choice === this.answer
+  }
+}
+
 const questions = [
   new Question(
     "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
@@ -20,3 +31,67 @@ const questions = [
     "Math.round()"
   ),
 ];
+
+class Quiz{
+  constructor(questions){
+    this.score=0
+    this.questions=questions
+    this.currentQuestionIndex=0
+  }
+  getCurrentQuestion(){
+    return this.questions[this.currentQuestionIndex]
+  }
+  guess(answer){
+    if (this.getCurrentQuestion().isCorrectAnswer(answer)){
+      this.score++
+    }
+    this.currentQuestionIndex++
+  }
+  hasEnded(){
+    return this.currentQuestionIndex >=this.questions.length;
+  }
+}
+
+// Quiz display
+
+const display= {
+  elementShow:function(id,text){
+    let element=document.getElementById(id);
+    element.innerHTML=text
+  },
+  question: function(){
+    this.elementShow("question",quiz.getCurrentQuestion().text)
+  },
+  choices:function(){
+    let choices =quiz.getCurrentQuestion().choices;
+
+    guessHandler=(id,guess)=>{
+      document.getElementById(id).onclick=function(){
+        quiz.guess(guess)
+        quizApp()
+      }
+    }
+    for (let i=0 ; i<choices.length; i++){
+      this.elementShow("choice" + i, choices[i])
+      guessHandler('guess' + i, choices[i])
+    }
+  }
+}
+
+
+//game logic
+
+quizApp=()=>{
+  if (quiz.hasEnded()) {
+    // ecran de fin
+  }else{
+    display.question();
+    display.choices()
+  }
+}
+
+
+// create quiz
+
+let quiz=new Quiz(questions)
+quizApp()
